2a8fdb315a197fbadbbb6cd12a6e1cd94df0d733
marcelo <marceloberlitz@outlook.com>
2019-07-02 13:39:09 -0300

    Corrige layout tela login

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
-import {MzInputModule} from 'ngx-materialize';
 
 @NgModule({
   declarations: [
     LoginComponent
   ],
   imports: [
-    CommonModule,
-    MzInputModule
+    CommonModule
   ]
 })
 export class LoginModule { }
